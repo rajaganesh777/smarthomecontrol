a749290e817e0f1c25f058e72f203ee06b21259e
rajaganesh777 <rajaganesh835@gmail.com>
2017-09-17 14:02:42 +0530

    Initial commit

---
+++ app/src/main/java/com/androidmkab/webview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.androidmkab.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    Button im;
+    TextView tv;
+    @Override
+   public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+tv =(TextView)findViewById(R.id.tv);
+        im =(Button) findViewById(R.id.swipe);
+        //swipe = (Button) findViewById(R.id.swipe);
+
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                webView = (WebView) findViewById(R.id.webview);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.getSettings().setAppCacheEnabled(true);
+                webView.loadUrl("http://192.168.4.1");
+              // final  EditText search_key = (EditText) findViewById (R.id.et);
+                //String key = .getText.ToString();
+
+
+                //WebView webview=(WebView)findViewById(R.id.webview);
+                //webView.loadUrl( "http://192.168.4.1/pin=" + key);
+
+
+            }
+        });
+
+
+    }
+
+
+
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+}
